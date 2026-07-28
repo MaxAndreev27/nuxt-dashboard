1199a30b097292b6ec423e268a61fcc9fe61318e
max <maxandreev27@gmail.com>
2026-07-28 17:41:03 +0300

    Add env with API url for fastapi backend

--- app/app.config.ts
+++ app/app.config.ts
@@ -4,5 +4,6 @@ export default defineAppConfig({
       primary: 'green',
       neutral: 'slate'
     }
-  }
+  },
+  apiBaseUrl: import.meta.env.VITE_API_BASE_URL || 'https://fastapi-course-example.fly.dev'
 })
